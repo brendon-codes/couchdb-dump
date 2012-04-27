61322dd1a151fe741572a2cbff2cb3b320b80b72
Brendon <425026+brendon-codes@users.noreply.github.com>
2012-04-27 00:28:29 -0700

    Initial fixes

--- couchdb-dump.py
+++ couchdb-dump.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
 
+"""
+CouchDB DB Dumper
+
+Tested On:
+
+  * Python 2.6
+  * Ubuntu 10.04 LTS
+  * CouchDB 1.1.1
+
+Requirements:
+
+  $ sudo apt-get install libyajl-dev libyajl1 yajl-tools
+  $ sudo pip install ijson
+"""
+
 import sys
 import logging
+import urllib2
+import urllib
 from optparse import OptionParser
 
-import requests
+import ijson
 
 
 logger = logging.getLogger()
@@ -28,28 +45,101 @@ def main():
     return 0
 
 
-class Dump(object):
+class RequestWithMethod(urllib2.Request):
+    """
+    Allows setting method in urllib2
 
-    src_url = None
+    See: http://abhinandh.com/post/2383952338
+         /making-a-http-delete-request-with-urllib2
+    """
+
+    def __init__(self, url, method, data=None, headers={},
+                 origin_req_host=None, unverifiable=False):
+        """
+        Init
+        """
+        self._method = method
+        urllib2.Request\
+               .__init__(self, url, data, headers,
+                         origin_req_host, unverifiable)
+
+    def get_method(self):
+        """
+        Get-Method hook
+        """
+        if self._method:
+            return self._method
+        else:
+            return urllib2.Request.get_method(self)
+
+
+class Requester(object):
+    """
+    Url Fetching Abstraction
+    """
+
+    def get(self, url, params=None):
+        """
+        GET request
+        """
+        return self.request(url, 'GET', params)
+
+    def request(self, url, method, params=None):
+        """
+        Run a request
+        """
+        u = url
+        p = None
+        if params is None:
+            params = {}
+        if method in ('GET', 'DELETE'):
+            if (len(params) > 0) and ('?' not in url):
+                parms = urllib.urlencode(params)
+                u = '?'.join((url, parms))
+        elif method in ('PUT', 'POST'):
+            p = urllib.urlencode(params)
+        else:
+            raise Exception('Method %s not supported' % method)
+        req = RequestWithMethod(u, method)
+        try:
+            res = urllib2.urlopen(req, p)
+        except urllib2.URLError:
+            raise Exception('Cannot reach server!')
+        return res
+
+
+class Dump(object):
+    """
+    Main db dumper
+    """
+
+    _src_url = None
+    _chunk_size = 3
 
     def __init__(self, src_url):
         """
         Init
         """
-        self.src_url = src_url
+        self._src_url = src_url.rstrip('/')
 
     def run(self):
         """
         Runner
         """
-        logger.info(self._path('_all_docs'))
+        url = self._path('_all_docs')
+        params = {
+            'limit': self._chunk_size
+        }
+        r = Requester().get(url, params)
+        ijson.items(r, 'rows.item')
+        print r.read()
         return True
 
     def _path(self, *args):
         """
         Get a path
         """
-        return '/'.join([self.src_url] + list(args))
+        return '/'.join([self._src_url] + list(args))
 
 
 if __name__ == '__main__':
